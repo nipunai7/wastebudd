497a9feefb3959a7b19a14b0f7ab157819227d92
Ruwanthi Thamali Perera <66712943+Ruwanthi7@users.noreply.github.com>
2020-07-08 13:05:43 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title>Waste Management System</title>
     <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+   
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
         
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" />
     <link href="assets/css/light-bootstrap-dashboard.css?v=2.0.0 " rel="stylesheet" />
