1bb5b68cbfcacd8d50329d69c98f18cc6377a172
Ruwanthi Thamali Perera <66712943+Ruwanthi7@users.noreply.github.com>
2020-07-11 23:29:39 +0530

    Add files via upload

---
+++ notifications.php
@@ -0,0 +1,232 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    
+   <link rel="icon"  href="https://img.icons8.com/dusk/2x/garbage-truck.png">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>Waste Management System</title>
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+        
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+        
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="../assets/css/light-bootstrap-dashboard.css?v=2.0.0 " rel="stylesheet" />
+    
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="sidebar" data-image="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQA2OVeThkfkmGbgVl7HOQUGcCPhWgXDrnHWQ&usqp=CAU" data-color = "blue">
+            
+
+            <div class="sidebar-wrapper">
+                <div class="logo">
+                    <a href="" class="simple-text">
+                        Waste Management System
+                    </a>
+                </div>
+                <ul class="nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">
+                            <i class="nc-icon nc-chart-pie-35"></i>
+                            <p>Dashboard</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="./user.php">
+                            <i class="nc-icon nc-circle-09"></i>
+                            <p>My Account</p>
+                        </a>
+                    </li>
+                    
+                    <li>
+                        <a class="nav-link" href="./maps.php">
+                            <i class="nc-icon nc-pin-3"></i>
+                            <p>Location</p>
+                        </a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="./notifications.php">
+                            <i class="nc-icon nc-bell-55"></i>
+                            <p>Notifications</p>
+                        </a>
+                    </li>
+                    
+                </ul>
+            </div>
+        </div>
+        <div class="main-panel">
+            
+            <nav class="navbar navbar-expand-lg bg-light " color-on-scroll="500">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#"> Dashboard </a>
+                    <button href="" class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-bar burger-lines"></span>
+                        <span class="navbar-toggler-bar burger-lines"></span>
+                        <span class="navbar-toggler-bar burger-lines"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-end" id="navigation">
+                        <ul class="nav navbar-nav mr-auto">
+                            <li class="dropdown nav-item">
+                                <a href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
+                                    <i class="nc-icon nc-bell-55"></i>
+                                    <span class="notification">3</span>
+                                    <span class="d-lg-none">Notifications</span>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <a class="dropdown-item" href="#">Garbage bin at Bandaragama town is full.</a>
+                                    <a class="dropdown-item" href="#">2 Garbage bins are full at Kelaniya.</a>
+                                    <a class="dropdown-item" href="#">Another Notification......</a>
+                                    
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    <i class="nc-icon nc-zoom-split"></i>
+                                    <span class="d-lg-block">&nbsp;Search</span>
+                                </a>
+                            </li>
+                        </ul>
+                        <ul class="navbar-nav ml-auto">
+                            
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <span class="no-icon">Select Area</span>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <a class="dropdown-item" href="#">Bandaragama</a>
+                                    <a class="dropdown-item" href="#">Hikkaduwa</a>
+                                    <a class="dropdown-item" href="#">Kelaniya</a>
+                                    <a class="dropdown-item" href="#">Colombo</a>
+                                    
+                                </div>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="user.html">
+                                    <span class="no-icon"> 
+                                        <button class="btn-outline-success btn-sm">Sign Up</button></span>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    <span class="no-icon"><button class="btn-outline-danger btn-sm">Log Out</button></span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <!-- End Navbar -->
+            <div class="content">
+                <div class="container-fluid">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">Notifications</h4>
+                            
+                        </div>
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                   
+                                    
+                                    
+                                    <div class="alert alert-warning alert-with-icon" data-notify="container">
+                                        <button type="button" aria-hidden="true" class="close" data-dismiss="alert">
+                                            <i class="nc-icon nc-simple-remove"></i>
+                                        </button>
+                                        <span data-notify="icon" class="nc-icon nc-bell-55"></span>
+                                        <span data-notify="message">You have not collected the garbage bin at Bandaragama town.</span>
+                                    </div>
+                                    <div class="alert alert-info alert-with-icon" data-notify="container">
+                                        <button type="button" aria-hidden="true" class="close" data-dismiss="alert">
+                                            <i class="nc-icon nc-simple-remove"></i>
+                                        </button>
+                                        <span data-notify="icon" class="nc-icon nc-bell-55"></span>
+                                        <span data-notify="message">You just now collected 5 garbage bins.</span>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <h5>
+                                        <small>Notification States</small>
+                                    </h5>
+                                    
+                                    <div class="alert alert-success">
+                                        <button type="button" aria-hidden="true" class="close" data-dismiss="alert">
+                                            <i class="nc-icon nc-simple-remove"></i>
+                                        </button>
+                                        <span>
+                                            <b> Success - </b> You reached your goal today!</span>
+                                    </div>
+                                    
+                                    <div class="alert alert-danger">
+                                        <button type="button" aria-hidden="true" class="close" data-dismiss="alert">
+                                            <i class="nc-icon nc-simple-remove"></i>
+                                        </button>
+                                        <span>
+                                            <b> Danger - </b> You are late! The garbage bin at Kelaniya is overloading!</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <br>
+                            <br>
+                            
+                                
+                            </div>
+                            <div class="row">
+                                <div class="col-md-12 text-center">
+                                    <h4 class="title"> For More Details</h4>
+                                    <a class="btn btn-info btn-fill btn-wd" data-toggle="modal" data-target="#myModal1" href="#pablo">
+                                        Click Here
+                                    </a>
+                                   <hr>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Mini Modal -->
+                    <div class="modal fade modal-mini modal-primary" id="myModal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                        <div class="modal-dialog bg-primary">
+                            <div class="modal-content">
+                                <div class="modal-header justify-content-center">
+                                    <div class="modal-profile">
+                                        <i class="nc-icon nc-bulb-63"></i>
+                                    </div>
+                                </div>
+                                <div class="modal-body text-center">
+                                    <p>Always try to collect overfilled garbage bins first. Have a nice day!</p>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-link btn-simple">Back</button>
+                                    <button type="button" class="btn btn-link btn-simple" data-dismiss="modal">Close</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--  End Modal -->
+                </div>
+            </div>
+           
+        </div>
+    </div>
+   </body>
+
+<script src="../assets/js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
+<script src="../assets/js/core/popper.min.js" type="text/javascript"></script>
+<script src="../assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+
+<script src="../assets/js/plugins/bootstrap-switch.js"></script>
+
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+
+<script src="../assets/js/plugins/chartist.min.js"></script>
+
+<script src="../assets/js/plugins/bootstrap-notify.js"></script>
+
+<script src="../assets/js/light-bootstrap-dashboard.js?v=2.0.0 " type="text/javascript"></script>
+
+<script src="../assets/js/demo.js"></script>
+
+</html>
