c3be675dbbf7c983af665398aada4f3d7d753533
Ruwanthi Thamali Perera <66712943+Ruwanthi7@users.noreply.github.com>
2020-09-20 11:31:59 +0530

    Update light-bootstrap-dashboard.js

--- assets/js/light-bootstrap-dashboard.js
+++ assets/js/light-bootstrap-dashboard.js
@@ -172,10 +172,8 @@ lbd = {
 
 
 
-// Returns a function, that, as long as it continues to be invoked, will not
-// be triggered. The function will be called after it stops being called for
-// N milliseconds. If `immediate` is passed, trigger the function on the
-// leading edge, instead of the trailing.
+
+
 
 function debounce(func, wait, immediate) {
     var timeout;
