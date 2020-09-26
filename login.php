38680a044825dd78e6916e42ad06ebb406b5678a
Ruwanthi Thamali Perera <66712943+Ruwanthi7@users.noreply.github.com>
2020-09-26 17:26:19 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en" >
+	<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta charset="utf-8">
+		<title> Login Page</title>
+		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
+	</head>
+	<body>
+
+		<div class="form">
+			
+			<form class="login-form" action="" method="post">
+				<i class="fas fa-user-circle"></i>
+				<input class="user-input" type="text" name="" placeholder="Username" required>
+				<input class="user-input" type="password" name="" placeholder="Password" required>
+				<div class="options-01">
+					<label class="remember-me"><input type="checkbox" name="">Remember me</label>
+					<a href="#">Forgot your password?</a>
+				</div>
+				<input class="btn" type="submit" name="" value="LOGIN">
+				<div class="options-02">
+					<p>Not Registered? &nbsp;<a href="#">Create an Account</a></p>
+				</div>
+			</form>
+			
+			<form class="signup-form" action="" method="post">
+				<i class="fas fa-user-plus"></i>
+				<input class="user-input" type="text" name="" placeholder="Username" required>
+				<input class="user-input" type="email" name="" placeholder="Email Address" required>
+				<input class="user-input" type="password" name="" placeholder="Password" required>
+				<input class="btn" type="submit" name="" value="SIGN UP">
+				<div class="options-02">
+					<p>Already Registered? <a href="#">Sign In</a></p>
+				</div>
+			</form>
+			
+		</div>
+		
+
+		<script type="text/javascript">
+		$('.options-02 a').click(function(){
+			$('form').animate({
+				height: "toggle", opacity: "toggle"
+			}, "slow");
+		});
+		</script>
+
+	</body>
+</html>
