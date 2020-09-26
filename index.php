fc87ed926b374eef8afac3c3aeec7a7bd96633a0
Ruwanthi Thamali Perera <66712943+Ruwanthi7@users.noreply.github.com>
2020-09-26 16:53:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 
 <body>
     <div class="wrapper">
-        <div class="sidebar"  data-color = "#111">
+        <div class="sidebar"  data-color = "navy blue">
             
 
             <div class="sidebar-wrapper">
