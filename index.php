d20f2b94c761de35da01269bcd18fe816388f9ca
Ruwanthi Thamali Perera <66712943+Ruwanthi7@users.noreply.github.com>
2020-09-29 21:00:15 +0530

    Update index.php

--- index.php
+++ index.php
@@ -211,16 +211,14 @@
 </body>
 
 
-    <script src="assets/js/jquery.3.2.1.min.js" type="text/javascript"></script>
-	<script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
-
-	<script src="assets/js/chartist.min.js"></script>
-
-   
-
-   <script src="assets/js/light-bootstrap-dashboard.js?v=1.4.0"></script>
-
-   <script src="assets/js/demo.js"></script>
+   <script src="../assets/js/core/jquery.3.2.1.min.js" type="text/javascript"></script>
+<script src="../assets/js/core/popper.min.js" type="text/javascript"></script>
+<script src="../assets/js/core/bootstrap.min.js" type="text/javascript"></script>
+<script src="../assets/js/plugins/bootstrap-switch.js"></script>
+<script src="../assets/js/plugins/chartist.min.js"></script>
+<script src="../assets/js/plugins/bootstrap-notify.js"></script>
+<script src="../assets/js/light-bootstrap-dashboard.js?v=2.0.0 " type="text/javascript"></script>
+<script src="../assets/js/demo.js"></script>
 <script type="text/javascript">
     $(document).ready(function() {
         
