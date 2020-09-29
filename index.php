dae576df843287adc48be8dc2285da5be6ab2f10
Ruwanthi Thamali Perera <66712943+Ruwanthi7@users.noreply.github.com>
2020-09-29 21:11:07 +0530

    Update index.php

--- index.php
+++ index.php
@@ -220,9 +220,10 @@
 <script src="../assets/js/light-bootstrap-dashboard.js?v=2.0.0 " type="text/javascript"></script>
 <script src="../assets/js/demo.js"></script>
 <script type="text/javascript">
-    $(document).ready(function() {
-        
-        demo.initDashboardPageCharts();
+   <script type="text/javascript">
+    	$(document).ready(function(){
+
+        	demo.initChartist();
 
          });
 </script>
