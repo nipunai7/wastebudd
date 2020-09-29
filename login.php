bf15b730c7f7042b8f9f0b67e04b3c373023b42e
Ruwanthi Thamali Perera <66712943+Ruwanthi7@users.noreply.github.com>
2020-09-29 22:51:34 +0530

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,9 @@
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta charset="utf-8">
-		<title> Login Page</title>
+		<link rel="icon"  href="https://img.icons8.com/dusk/2x/garbage-truck.png">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+                <title>Waste Management System</title>
 		<link rel="stylesheet" href="style.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
