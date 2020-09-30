8a9706c0faeb253031b6215f6276c520f4e3bc53
Ruwanthi Thamali Perera <66712943+Ruwanthi7@users.noreply.github.com>
2020-09-30 22:36:22 +0530

    Update login.php

--- login.php
+++ login.php
@@ -11,21 +11,11 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
 	</head>
 	<body>
-		<div>
+		
 		<div class="waveWrapper waveAnimation">
   <div class="waveWrapperInner bgTop">
     <div class="wave waveTop" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-top.png')"></div>
-  </div>
-  <div class="waveWrapperInner bgMiddle">
-    <div class="wave waveMiddle" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-mid.png')"></div>
-  </div>
-  <div class="waveWrapperInner bgBottom">
-    <div class="wave waveBottom" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-bot.png')"></div>
-	  
-  </div>
-			
-</div>
-<div class="form">
+	  <div class="form">
 			
 			<form class="login-form" action="logindb.php" method="post">
 				<i class="fas fa-user-circle"></i>
@@ -55,9 +45,20 @@
 			</form>
 			
 		</div>	
-		</div>
 		
 		
+		
+  </div>
+  <div class="waveWrapperInner bgMiddle">
+    <div class="wave waveMiddle" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-mid.png')"></div>
+  </div>
+  <div class="waveWrapperInner bgBottom">
+    <div class="wave waveBottom" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-bot.png')"></div>
+	  
+  </div>
+			
+</div>
+
 
 		<script type="text/javascript">
 		$('.options-02 a').click(function(){
