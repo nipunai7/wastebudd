3056041910aceb6d20410c2657580dfe9287108b
Ruwanthi Thamali Perera <66712943+Ruwanthi7@users.noreply.github.com>
2020-09-30 23:23:14 +0530

    Update login.php

--- login.php
+++ login.php
@@ -6,7 +6,8 @@
 		<link rel="icon"  href="https://img.icons8.com/dusk/2x/garbage-truck.png">
                 <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
                 <title>Waste Management System</title>
-		<link rel="stylesheet" href="style.css">
+		<link rel="stylesheet" href="style.css"><link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+		
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
 	</head>
@@ -15,7 +16,8 @@
 		<div class="waveWrapper waveAnimation">
   <div class="waveWrapperInner bgTop">
     <div class="wave waveTop" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-top.png')"></div>
-	  <div class="form">
+	 <div class = "card">
+		 <div class="form">
 			
 			<form class="login-form" action="logindb.php" method="post">
 				<i class="fas fa-user-circle"></i>
@@ -56,7 +58,7 @@
     <div class="wave waveBottom" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-bot.png')"></div>
 	  
   </div>
-			
+			</div>		
 </div>
 
 
