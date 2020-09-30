7e7c2019e0a4f14956d78e649c0147fb1009b7ee
Ruwanthi Thamali Perera <66712943+Ruwanthi7@users.noreply.github.com>
2020-09-30 21:29:43 +0530

    Update login.php

--- login.php
+++ login.php
@@ -11,6 +11,17 @@
 		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" charset="utf-8"></script>
 	</head>
 	<body>
+		<div class="waveWrapper waveAnimation">
+  <div class="waveWrapperInner bgTop">
+    <div class="wave waveTop" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-top.png')"></div>
+  </div>
+  <div class="waveWrapperInner bgMiddle">
+    <div class="wave waveMiddle" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-mid.png')"></div>
+  </div>
+  <div class="waveWrapperInner bgBottom">
+    <div class="wave waveBottom" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-bot.png')"></div>
+  </div>
+</div>
 
 		<div class="form">
 			
