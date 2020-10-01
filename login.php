cdd0bb3affd73917fa94b5d973ca54d0b1ca3e38
Ruwanthi Thamali Perera <66712943+Ruwanthi7@users.noreply.github.com>
2020-10-01 11:39:32 +0530

    Update login.php

--- login.php
+++ login.php
@@ -15,39 +15,13 @@
 		
 		<div class="waveWrapper waveAnimation">
   <div class="waveWrapperInner bgTop">
-	   <button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Login</button>
-	  <div id="id01" class="modal">
-  
-  <form class="modal-content animate" action="/action_page.php" method="post">
-    <div class="imgcontainer">
-      <span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
-      <img src="img_avatar2.png" alt="Avatar" class="avatar">
-    </div>
-
-    <div class="container">
-      <label for="uname"><b>Username</b></label>
-      <input type="text" placeholder="Enter Username" name="uname" required>
-
-      <label for="psw"><b>Password</b></label>
-      <input type="password" placeholder="Enter Password" name="psw" required>
-        
-      <button type="submit">Login</button>
-      <label>
-        <input type="checkbox" checked="checked" name="remember"> Remember me
-      </label>
-    </div>
-
-    <div class="container" style="background-color:#f1f1f1">
-      <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-      <span class="psw">Forgot <a href="#">password?</a></span>
-    </div>
-  </form>
-</div>
-	 
+	  
 		
 		
   </div>
-			 <div class="form" >
+
+    <div class="wave waveTop" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-top.png')"></div>
+				 <div class="form" >
 			
 			<form class="login form" action="dblogin.php" method="post">
 				<i class="fas fa-user-circle"></i>
@@ -78,8 +52,6 @@
 			
 			
 		</div>	
-    <div class="wave waveTop" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-top.png')"></div>
-	
 		 
   <div class="waveWrapperInner bgMiddle">
 	 
