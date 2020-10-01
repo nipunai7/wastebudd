9f1a010dc2967633ea6f82529afa80562a8f7b9f
Ruwanthi Thamali Perera <66712943+Ruwanthi7@users.noreply.github.com>
2020-10-01 12:03:29 +0530

    Update login.php

--- login.php
+++ login.php
@@ -13,55 +13,16 @@
 	</head>
 	<body>
 		
-		<div class="waveWrapper waveAnimation">
+<div class="waveWrapper waveAnimation">
   <div class="waveWrapperInner bgTop">
-	  <div class="wave waveTop" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-top.png')"></div>
-				 <div class="form" >
-			
-			<form class="login form" action="dblogin.php" method="post">
-				<i class="fas fa-user-circle"></i>
-				<input class="user-input" type="text" name="" placeholder="Username" required>
-				<input class="user-input" type="email" name="" placeholder="Email Address" required>
-				<input class="user-input" type="password" name="" placeholder="Password" required>
-				<div class="options-01">
-					<label class="remember-me"><input type="checkbox" name="">Remember me</label>
-					<a href="#">Forgot your password?</a>
-				</div>
-				<input class="btn" type="submit" name="" value="LOGIN" href="index.php">
-				<div class="options-02">
-					<p>Not Registered? &nbsp;<a href="#">Create an Account</a></p>
-				</div>
-			</form>
-			
-			<form class="signup-form" action="regdb.php" method="post">
-				<i class="fas fa-user-plus"></i>
-				<input class="user-input" type="text" name="" placeholder="Username" required>
-				<input class="user-input" type="email" name="" placeholder="Email Address" required>
-				<input class="user-input" type="password" name="" placeholder="Password" required>
-				<input class="user-input" type="password" name="" placeholder="Confirm Password" required>
-				<input class="btn" type="submit" name="" value="SIGN UP">
-				<div class="options-02">
-					<p>Already Registered? <a href="#">Sign In</a></p>
-				</div>
-			</form>
-			
-			
-		</div>	
-		 
-	  
-<
-    
+    <div class="wave waveTop" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-top.png')"></div>
+  </div>
   <div class="waveWrapperInner bgMiddle">
-	 
-		
-
     <div class="wave waveMiddle" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-mid.png')"></div>
   </div>
   <div class="waveWrapperInner bgBottom">
     <div class="wave waveBottom" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-bot.png')"></div>
-	  
   </div>
-				
 </div>
 		
 
@@ -77,17 +38,7 @@
 		<script>
 		var form = document.getElementById('login');
 		</script>
-		<script>
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-</script>
+	
 
 	</body>
 </html>
