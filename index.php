90287a436489662aba135d59628cfb1b5ff33313
Ruwanthi Thamali Perera <66712943+Ruwanthi7@users.noreply.github.com>
2021-07-06 14:27:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -188,9 +188,9 @@
                                     
                                 </div>
                             </div>
-				<iframe width="1140" height="500.25" 
+				<iframe width="940" height="500.25" 
 					src="https://app.powerbi.com/reportEmbed?reportId=40a1b408-f339-4479-98b1-87dbcef4e8be&autoAuth=true&ctid=84c31ca0-ac3b-4eae-ad11-519d80233e6f&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtdXMtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D"
-					frameborder="1" allowFullScreen="true"></iframe>
+					frameborder="0" allowFullScreen="true"></iframe>
                         </div>
                         <div class="col-md-6">
                             <div class="card  card-tasks">
