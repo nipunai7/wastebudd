2f89ee9f770fb9fc202f1c77e8356e80afd13217
Ruwanthi Thamali Perera <66712943+Ruwanthi7@users.noreply.github.com>
2021-07-06 14:35:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -125,7 +125,7 @@
             <div class="content">
                 <div class="container-fluid">
                     <div class="row">
-			    <div>
+			    <div class ="row>
 				<iframe width="1340" height="650.25" 
 					src="https://app.powerbi.com/reportEmbed?reportId=40a1b408-f339-4479-98b1-87dbcef4e8be&autoAuth=true&ctid=84c31ca0-ac3b-4eae-ad11-519d80233e6f&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtdXMtcmVkaXJlY3QuYW5hbHlzaXMud2luZG93cy5uZXQvIn0%3D"
 					frameborder="0" allowFullScreen="true"></iframe>
@@ -149,9 +149,7 @@
                                     </div>
                                 </div></div>
 				
-                        </div>
-                            </div>
-                        </div>
+                        
                         <div class="col-md-8">
                             <div class="card ">
                                 <div class="card-header ">
